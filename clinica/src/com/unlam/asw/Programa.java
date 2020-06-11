0ad43b2ce81e4c39c82c56495240885716641976
Joaquín Krasuk <joaquin.krasuk@albany.company>
2020-06-11 19:03:57 -0300

    Revert "V2"
    This reverts commit bc1c98928302083e39bbbf06273e1684c15743c4.

---
+++ clinica/src/com/unlam/asw/Programa.java
@@ -0,0 +1,138 @@
+package com.unlam.asw;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+import com.unlam.asw.DB.DAO;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Programa {
+	private DAO dao;
+	private JFrame frame;
+	private JTextField txtCodPaciente;
+	private JTextField txtNombrePaciente;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Programa window = new Programa();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Programa() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setResizable(false);
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		JLabel lblCodPaciente = new JLabel("C\u00F3digo");
+		lblCodPaciente.setBounds(95, 50, 65, 14);
+		panel.add(lblCodPaciente);
+		
+		txtCodPaciente = new JTextField();
+		txtCodPaciente.setBounds(166, 47, 162, 20);
+		panel.add(txtCodPaciente);
+		txtCodPaciente.setColumns(10);
+		
+		JLabel lblAltaDePaciente = new JLabel("Alta de Paciente");
+		lblAltaDePaciente.setBounds(173, 14, 151, 14);
+		panel.add(lblAltaDePaciente);
+		
+		txtNombrePaciente = new JTextField();
+		txtNombrePaciente.setBounds(166, 97, 162, 20);
+		panel.add(txtNombrePaciente);
+		txtNombrePaciente.setColumns(10);
+		
+		JLabel lblNombreDelPaciente = new JLabel("Nombre");
+		lblNombreDelPaciente.setBounds(95, 99, 64, 14);
+		panel.add(lblNombreDelPaciente);
+		
+		JButton btnConfirmar = new JButton("Confirmar");
+		btnConfirmar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {				
+				generarAltaPaciente();
+			}
+		});
+		btnConfirmar.setBounds(170, 201, 118, 23);
+		panel.add(btnConfirmar);
+
+		dao = new DAO();
+	}
+	
+	private void generarAltaPaciente() {
+		String strCod = txtCodPaciente.getText().trim();
+		String strNombre = txtNombrePaciente.getText().trim();
+		int nombreLength = strNombre.length();
+		
+		//Validacion de parse-int
+		if (esCodigoValido(strCod))
+		{
+			int cod = Integer.parseInt(strCod);			
+			
+			//Busca el paciente en una query, si existe devuelve true
+			if (!existePaciente(cod)) {
+				//Si el paciente no existe, chequeamos que se haya ingresado bien el nombre
+				if (nombreLength <= 50 && nombreLength > 0) {					
+					registrarPaciente(cod, strNombre);
+					JOptionPane.showMessageDialog(null, "Paciente registrado con éxito en la base de datos.", "Paciente registrado", JOptionPane.INFORMATION_MESSAGE);
+				} else {
+					JOptionPane.showMessageDialog(null, "El nombre ingresado excede el límite de 50 caracteres, o está vacío.", "Paciente registrado", JOptionPane.INFORMATION_MESSAGE);
+				}
+			} else {
+				JOptionPane.showMessageDialog(null, "¡El paciente ya existe!", "Error", JOptionPane.INFORMATION_MESSAGE);
+			}			
+		} else {
+			JOptionPane.showMessageDialog(null, "¡El código ingresado no es válido!", "Error", JOptionPane.INFORMATION_MESSAGE);
+		}				
+	}
+	
+	public boolean esCodigoValido(String codigo) {		
+		try {
+			int cod = Integer.parseInt(codigo);
+			return true;
+		}	
+		 catch (Exception e) {
+			return false;
+		}
+	}
+	
+	public boolean existePaciente(int cod) {
+		//TODO: conexion bd y query paciente
+		return false;
+	}
+	
+	public void registrarPaciente(int cod, String strNombre) {
+		//TODO: conexion bd, insert paciente
+	}
+}
---
+++ clinica/src/com/unlam/asw/entities/Paciente.java
@@ -0,0 +1,78 @@
+package com.unlam.asw.entities;
+
+public class Paciente {
+	private int codigo;
+	private String nombre;
+
+	public Paciente(String codigo, String nombre) throws Exception {
+		setCodigo(codigo);
+		setNombre(nombre);
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	/**
+	 * Setter del código del paciente. El código debe ser un número entero. 
+	 * @param codigo (string) cadena que será convertida a entero. 
+	 * @throws Exception  En caso de no serlo arroja una excepción. En caso de recibir una cadena vacía, arroja excepción.
+	 */
+	public final void setCodigo(String codigo) throws Exception {
+		try {
+			//Chequeamos no recibir una cadena nula ni una cadena que solo contenga espacios en blanco,
+			//caso contrario arrojamos una excepcion de formato
+			if (codigo == null || codigo.trim().isEmpty())
+				throw new NumberFormatException();			
+			//Convertimos la cadena a un entero y lo asignamos al atributo codigo del objeto
+			this.codigo = Integer.parseInt(codigo);
+		} catch (NumberFormatException e) {
+			throw new Exception("El codigo del paciente debe ser un número entero.");
+		}
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	/**
+	 * Setter del nombre. Verifica que el nombre contenga solamente caracteres válidos.
+	 * @param nombre (string) cadena de texto que será asignada como nombre al paciente, en caso de pasar la validación. 
+	 * @throws Exception Arroja excepciones en caso de que el nombre contenga caracteres no válidos o en caso de que la cadena recibida sea nula.
+	 */
+	public final void setNombre(String nombre) throws Exception {
+		//Chequeamos que la cadena recibida no sea nula, y además chequeamos que no hayamos recibido
+		//una cadena con espacios en blanco
+		if (nombre != null && !nombre.trim().isEmpty()) {
+			//Ejecutamos el metodo soloCaracteres en la cadena para chequear que contenga unicamente caracteres validos
+			if (this.soloCaracteres(nombre))
+				this.nombre = nombre;
+			else
+				throw new Exception("El paciente debe contener caracteres válidos!");
+		} else {
+			throw new Exception("El paciente debe tener un nombre!");
+		}
+	}
+
+	/**
+	 * Chequea que la cadena contenga solo caracteres válidos, considerando caracteres válidos
+	 * aquellos que sean letras y que no sean whiteSpace (tabulaciones, linefeed, espacios en blanco, etc). 
+	 * Los números no son aceptados. Solo se aceptan letras. 
+	 * 
+	 * Retorna true en caso de que no hayan caracteres no validos, y false en caso de que sí hayan.
+	 * 
+	 * @param cadena (string) cadena que será comprobada en busca de caracteres no válidos.
+	 */
+	private boolean soloCaracteres(String cadena) {
+		//Recorremos la cadena de principio a fin
+		for (int i = 0; i != cadena.length(); ++i) {
+			//isLetter chequea que la letra sea una letra unicode válida
+			if (!Character.isLetter(cadena.charAt(i)))
+				//Si el caracter no es una letra, retornamos falso
+				return false;
+		}
+		//Si no se encontraron caracteres no validos, retornamos true
+		return true;
+	}
+}
