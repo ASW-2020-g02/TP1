e7804a00c98a3a46febeab730475e016a063fb26
Joaquín Krasuk <joaquin.krasuk@albany.company>
2020-06-12 00:19:19 -0300

    Ingreso de datos

--- clinica/src/com/unlam/asw/pantallas/general/JIngresos.java
+++ clinica/src/com/unlam/asw/pantallas/general/JIngresos.java
@@ -2,6 +2,7 @@ package com.unlam.asw.pantallas.general;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -41,17 +42,20 @@ public class JIngresos extends JFrame {
 	public JIngresos() {
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 268);
+		setBounds(100, 100, 429, 404);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-
+		setTitle("Ingreso de datos");
+		
 		JPanel panel = new JPanel();
 		contentPane.add(panel, BorderLayout.CENTER);
 		panel.setLayout(null);
 
-		JButton btnDatosPaciente = new JButton("Datos del paciente");
+		JButton btnDatosPaciente = new JButton("<html><center>Datos del paciente</center></html>");
+		btnDatosPaciente.setFont(new Font("Tahoma", Font.BOLD, 17));
+		btnDatosPaciente.setFocusPainted(false);
 		btnDatosPaciente.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				JDatosPaciente dp = new JDatosPaciente();
@@ -59,10 +63,12 @@ public class JIngresos extends JFrame {
 				dispose();
 			}
 		});
-		btnDatosPaciente.setBounds(148, 23, 140, 40);
+		btnDatosPaciente.setBounds(97, 56, 240, 68);
 		panel.add(btnDatosPaciente);
 
-		JButton btnSituPaciente = new JButton("Situaci\u00F3n del paciente");
+		JButton btnSituPaciente = new JButton("<html><center>Situaci\u00F3n del paciente</center></html>");
+		btnSituPaciente.setFont(new Font("Tahoma", Font.BOLD, 17));
+		btnSituPaciente.setFocusPainted(false);
 		btnSituPaciente.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				JSituacionPaciente jp = new JSituacionPaciente();
@@ -70,10 +76,12 @@ public class JIngresos extends JFrame {
 				dispose();
 			}
 		});
-		btnSituPaciente.setBounds(148, 74, 140, 40);
+		btnSituPaciente.setBounds(97, 135, 240, 68);
 		panel.add(btnSituPaciente);
 
-		JButton btnDatosDelMdico = new JButton("Datos del m\u00E9dico");
+		JButton btnDatosDelMdico = new JButton("<html><center>Datos del m\u00E9dico</center></html>");
+		btnDatosDelMdico.setFont(new Font("Tahoma", Font.BOLD, 17));
+		btnDatosDelMdico.setFocusPainted(false);
 		btnDatosDelMdico.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				JDatosMedico jd = new JDatosMedico();
@@ -81,19 +89,21 @@ public class JIngresos extends JFrame {
 				dispose();
 			}
 		});
-		btnDatosDelMdico.setBounds(148, 125, 140, 40);
+		btnDatosDelMdico.setBounds(97, 214, 240, 68);
 		panel.add(btnDatosDelMdico);
 
-		JButton btnSalir = new JButton("Salir");
-		btnSalir.addActionListener(new ActionListener() {
+		JButton btnAnterior = new JButton("Volver");
+		btnAnterior.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				JInicial inicial = new JInicial();
 				inicial.setVisible(true);
 				dispose();
 			}
 		});
-		btnSalir.setBounds(148, 176, 140, 40);
-		panel.add(btnSalir);
+		btnAnterior.setBounds(329, 332, 74, 22);
+		btnAnterior.setFocusPainted(false);
+		panel.add(btnAnterior);
+
 		setLocationRelativeTo(null);
 	}
 
